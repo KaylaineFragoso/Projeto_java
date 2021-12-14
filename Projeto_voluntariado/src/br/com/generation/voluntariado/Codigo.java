0ca21a0b36478125a84a6361f362098d7df6358f
Luiz Alberto Melin <luizmelin34@gmail.com>
2021-12-13 22:45:24 -0300

    adicionando voluntariados ao litoral

--- Projeto_voluntariado/src/br/com/generation/voluntariado/Codigo.java
+++ Projeto_voluntariado/src/br/com/generation/voluntariado/Codigo.java
@@ -6,7 +6,7 @@ public class Codigo {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-        int teste, teste2, teste3, teste4, teste5;
+        int teste, teste2, teste3, teste4, teste5, teste6;
 
         System.out.println("Você gostaria de encontrar uma instituição para se voluntariar? \n1: Sim \n2: Não ");
         teste=entrada.nextInt();
@@ -150,9 +150,59 @@ public class Codigo {
 
 
             else if(teste2==2) {
-                System.out.println("essas são a opção do Litoral");
-                //Escolha do litoral dentro do 2 if                
+            	  //Escolha do litoral dentro do 2 if                
                 //
+            	System.out.println("[1]Entrega por Santos \n[2]Lar vicentino \n[3]Ecofaxina \n[4]Brinquedos do Bem \n[5]Supereco \n[6]Associação viva e deixe viver contadores de histórias");
+            	teste6=entrada.nextInt();
+            	if(teste6==1) {
+            		System.out.println("Razão social: Entrega por Santos\r\n"
+            				+ "Descrição: O projeto Entrega por Santos é uma mobilização solidária, sem fins lucrativos ou vínculos políticos, que busca resgatar sorrisos daqueles que vivem nas ruas de Santos e São Vicente. Conheça mais sobre o projeto e seja um voluntario!\r\n"
+            				+ "http://www.instagram.com/entregaporsantos\r\n"
+            				+ "Email entregaporsantos@gmail.com\r\n"
+            				+ "https://pt-br.facebook.com/entregaporsantos/");
+            	}
+            	else if(teste6==2) {
+            		System.out.println("Razão social : Lar vicentino \r\n"
+            				+ "Descrição: O lar procura pessoas para conversar com os idosos residentes. Se você tem paciência e espírito jovem, é bem-vindo! \r\n"
+            				+ "Tel (13) 3466-3331. \r\n"
+            				+ "O endereço é Rua Carijós, 139, em São Vicente.\r\n"
+            				+ "Conheça mais sobre o projeto e seja um voluntario!\r\n"
+            				+ "https://www.facebook.com/larvicentinos");
+            	}
+            	else if(teste6==3) {
+            		System.out.println("Razão social : Ecofaxina\r\n"
+            				+ "Descrição: O Instituto EcoFaxina atua desde 2008 com foco no desenvolvimento e apoio a políticas públicas, pesquisas, programas e ações voltadas à proteção do Meio Ambiente e ao Desenvolvimento Sustentável. Seus esforços estão focados na expansão da consciência ambiental.\r\n"
+            				+ "Rua Nabuco de Araújo, 71, Boqueirão\r\n"
+            				+ "Santos (SP) - CEP: 11025-010\r\n"
+            				+ "https://www.instagram.com/ecofaxina/\r\n"
+            				+ "https://www.institutoecofaxina.org.br/voluntariado-beta");
+            	}
+            	else if(teste6==4) {
+            		System.out.println("Razão social: Brinquedos do bem \r\n"
+            				+ "Descrição:  O grupo tem como objetivo levar brinquedos para as crianças internadas nos hospitais da cidade. O mais legal é que, além de espalhar sorrisos, ainda há um ensinamento: repassar os brinquedos:\r\n"
+            				+ "Tel para contato (13) 99719-3069 (13) 99110-9398\r\n"
+            				+ "Conheça mais sobre o projeto e seja um voluntario!\r\n"
+            				+ "https://www.facebook.com/Brinquedos-do-Bem-1474359839529099/");
+            	}
+            	else if(teste6==5) {
+            		System.out.println("Razão social: Supereco \r\n"
+            				+ "Descrição: Você tem algum talento e um propósito de vida? Quer viver uma experiência única e especial? Gosta das causas ambientais e sociais? Quer ensinar algo e aprender, compartilhando saberes com a comunidade?  Pode doar seu tempo para fazer a diferença?\r\n"
+            				+ "Conheça mais sobre o projeto e seja um voluntario!\r\n"
+            				+ "Rua Maria Cristiano, 72 - São Francisco\r\n"
+            				+ "São Sebastião - SP\r\n"
+            				+ "CEP 11629-263\r\n"
+            				+ "Email: administrativo@supereco.org.br\r\n"
+            				+ "https://www.supereco.org.br/voluntariado-1");
+            	}
+            	else if(teste6==6) {
+            		System.out.println("Razão social: Associação viva e deixe viver contadores de histórias\r\n"
+            				+ "Descrição: Levar a leitura, a brincadeira, as histórias para onde quer que vamos é o primeiro passo para transformar o atendimento clínico e internação hospitalar de crianças e adolescentes que passam por momentos difíceis em um momento mais alegre, saudável, agradável e terapêutico.\r\n"
+            				+ "https://www.instagram.com/vivavdv/\r\n"
+            				+ "https://www.vivaedeixeviver.org.br/seja-um-voluntario/");
+            	}
+            	else {
+            		System.out.println("Você digitou um numero invalido");
+            	}
             }
             else if(teste2==3) {
                 System.out.println("essas são a opção do Interior");
