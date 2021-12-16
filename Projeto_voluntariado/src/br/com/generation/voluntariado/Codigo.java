042f4b52c653648b37c8d6b54950854c1e0be5a4
Luiz Alberto Melin <luizmelin34@gmail.com>
2021-12-16 18:16:14 -0300

    adicionando array

--- Projeto_voluntariado/src/br/com/generation/voluntariado/Codigo.java
+++ Projeto_voluntariado/src/br/com/generation/voluntariado/Codigo.java
@@ -14,7 +14,6 @@ public class Codigo {
 		
 		Scanner entrada = new Scanner(System.in);
         int voluntariar, voluntariarReg, voluntariarSp;
-        
         Norte a5 = new Norte();
         Teste_sul a1 = new Teste_sul();
         Leste a2 = new Leste();
@@ -22,19 +21,18 @@ public class Codigo {
         Litoral a3 = new Litoral();
         Interior a6 = new Interior();
         Abc a4 = new Abc();
+        Array a8 = new Array(); 
         
         
         System.out.println("\nVocê gostaria de encontrar uma instituição para se voluntariar? \n1: Sim \n2: Não ");
         voluntariar=entrada.nextInt();
         
         if(voluntariar==1) {
-             System.out.println("Escolha uma região do estado de São Paulo:"
-                    + " \n[1] Capital\n[2] Litoral\n[3] Interior\n[4] ABC");
+             a8.lista();
             
         voluntariarReg=entrada.nextInt();
         if(voluntariarReg==1) {   
-        	 System.out.println("Escolha uma região da capital de São Paulo: "
-                        + "\n[1] Zona Norte \n[2] Zona Sul \n[3] Zona Leste \n[4] Zona Oeste");
+        	 a8.regiao();
         	    
         voluntariarSp=entrada.nextInt();
         if(voluntariarSp==1) {
@@ -84,6 +82,7 @@ public class Codigo {
         }
         else {
             System.out.println("Você digitou um numero invalido");
+            
         }
         
         entrada.close();
